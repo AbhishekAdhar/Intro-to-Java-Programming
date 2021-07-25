e55bb8b3b9c5b472078f54a0a3278019f4b9e85a
AbhishekAdhar <56471551+AbhishekAdhar@users.noreply.github.com>
2021-07-26 00:28:00 +0530

    Update Exercise_04_12.java
    I have made following Changes:
    -> Program now takes small alphabets into consideration too.
    -> Program wasn't working for numbers so added required changes(changes in if condition, numbers as character in cases).

--- Exercise_04/Exercise_04_12/Exercise_04_12.java
+++ Exercise_04/Exercise_04_12/Exercise_04_12.java
@@ -20,27 +20,33 @@ public class Exercise_04_12 {
 
 		// Display binary number for the hex digit
 		char ch = hexString.charAt(0);
-		if ((ch >= 'A' && ch <= 'F') || (ch >= 0 && ch <= 9))
+		if ((ch >= 'A' && ch <= 'F') || (ch >= 48 && ch <= 57) || (ch>='a' && ch<='f'))
 		{
 			System.out.print("The binary value is ");
 			switch(ch)
 			{
-				case 0: System.out.println(0000); break; 
-				case 1: System.out.println(0001); break; 
-				case 2: System.out.println(0010); break; 
-				case 3: System.out.println(0011); break; 
-				case 4: System.out.println(0100); break; 
-				case 5: System.out.println(0101); break; 
-				case 6: System.out.println(0110); break; 
-				case 7: System.out.println(0111); break; 
-				case 8: System.out.println(1000); break; 
-				case 9: System.out.println(1001); break; 
-				case 'A': System.out.println(1010); break; 
-				case 'B': System.out.println(1011); break; 
-				case 'C': System.out.println(1100); break; 
-				case 'D': System.out.println(1101); break; 
-				case 'E': System.out.println(1110); break; 
-				case 'F': System.out.println(1111); break; 
+				case '0': System.out.println("0000"); break; 
+				case '1': System.out.println("0001"); break; 
+				case '2': System.out.println("0010"); break; 
+				case '3': System.out.println("0011"); break; 
+				case '4': System.out.println("0100"); break; 
+				case '5': System.out.println("0101"); break; 
+				case '6': System.out.println("0110"); break; 
+				case '7': System.out.println("0111"); break; 
+				case '8': System.out.println("1000"); break; 
+				case '9': System.out.println("1001"); break;
+                		case 'a': 
+				case 'A': System.out.println("1010"); break;
+                		case 'b':
+				case 'B': System.out.println("1011"); break;
+                		case 'c':
+				case 'C': System.out.println("1100"); break; 
+                		case 'd':
+				case 'D': System.out.println("1101"); break;
+                		case 'e': 
+				case 'E': System.out.println("1110"); break;
+                		case 'f': 
+				case 'F': System.out.println("1111"); break;
 			}
 		}
 		else
